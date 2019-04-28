7b02f6b26734fc1fe1367bb280ff1c8412a9e175
KochiyaOcean <root@kochiyaocean.org>
2019-04-28 23:34:34 +0900

    for issue of webview nested iframe memory leak

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var BrowserWindow = require('electron').BrowserWindow;
 var app = require('electron').app;
 app.on('ready', function() {
   var mainWindow;
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      plugins: true,
+      webviewTag: true,
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+    },
+  });
   mainWindow.loadURL("file://"+ __dirname + "/index.html");
   mainWindow.openDevTools({detach: true});
 });
