8faa67c7c157a9c29625ab1a655233839dbac39c
東風谷オーシャン <root@kochiyaocean.org>
2021-10-28 23:31:35 +0900

    for issue of contextIsolation disabled webview behavior

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var BrowserWindow = require('electron').BrowserWindow;
 var app = require('electron').app;
+app.commandLine.appendSwitch('disable-site-isolation-trials', true)
 app.on('ready', function() {
   var mainWindow;
   mainWindow = new BrowserWindow({
@@ -8,8 +9,31 @@ app.on('ready', function() {
       webviewTag: true,
       nodeIntegration: true,
       nodeIntegrationInWorker: true,
+      nativeWindowOpen: true,
+      // enableRemoteModule: true,
+      contextIsolation: false,
+      // spellcheck: false,
     },
   });
   mainWindow.loadURL("file://"+ __dirname + "/index.html");
   mainWindow.openDevTools({detach: true});
+  mainWindow.webContents.addListener('new-window', (e, url, frameName, disposition, options, additionalFeatures) => {
+    e.preventDefault()
+    options = {
+      ...options,
+      minWidth: 200,
+      minHeight: 200,
+      webPreferences: {
+        nodeIntegration: false,
+        nodeIntegrationInWorker: false,
+        enableRemoteModule: true,
+        plugins: true,
+        sandbox: false,
+        webviewTag: true,
+        contextIsolation: false,
+      },
+    }
+    const win = new BrowserWindow(options)
+    e.newGuest = win
+  })
 });
